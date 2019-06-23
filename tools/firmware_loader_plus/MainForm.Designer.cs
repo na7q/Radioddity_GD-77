6100e5b8bcc8eaf98b95685598b34b269e528bf0
Roger Clark <roger@rogerclark.net>
2019-06-23 17:19:34 +1000

    Add new firmware loader GUI and new XML so that in the future the firmware loader can download directly from github

---
+++ tools/firmware_loader_plus/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel.Design;
+using System.Windows.Forms;
+using System;
+namespace GD77_FirmwareLoaderPlus
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOpenFile = new System.Windows.Forms.Button();
+			this.lblMessage = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// btnOpenFile
+			// 
+			this.btnOpenFile.Location = new System.Drawing.Point(88, 78);
+			this.btnOpenFile.Name = "btnOpenFile";
+			this.btnOpenFile.Size = new System.Drawing.Size(144, 23);
+			this.btnOpenFile.TabIndex = 0;
+			this.btnOpenFile.Text = "Select file and upload";
+			this.btnOpenFile.UseVisualStyleBackColor = true;
+			this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+			// 
+			// lblMessage
+			// 
+			this.lblMessage.Location = new System.Drawing.Point(13, 13);
+			this.lblMessage.Name = "lblMessage";
+			this.lblMessage.Size = new System.Drawing.Size(289, 31);
+			this.lblMessage.TabIndex = 1;
+			this.lblMessage.Text = "Please select a firmware file";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 47);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(290, 15);
+			this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			this.progressBar1.TabIndex = 2;
+			this.progressBar1.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(314, 113);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.lblMessage);
+			this.Controls.Add(this.btnOpenFile);
+			this.KeyPreview = true;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "GD-77 Firmware Loader Plus";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Button btnOpenFile;
+		private Label lblMessage;
+		private ProgressBar progressBar1;
+
+
+	}
+}
+
---
+++ tools/firmware_loader_plus/MainForm.cs
@@ -0,0 +1,133 @@
+﻿/*
+ * Copyright (C)2019 Roger Clark. VK3KYY
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. The name of the author may not be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel.Design;
+using System.IO;
+using UsbLibrary;
+using System.Threading;
+
+namespace GD77_FirmwareLoaderPlus
+{
+	public partial class MainForm : Form
+	{
+		private string _filename="";
+		public MainForm(string[] args)
+		{
+			InitializeComponent();
+			this.CenterToScreen();
+			this.BringToFront();
+			this.Refresh();
+			if (args.Length > 0)
+			{
+				try
+				{
+					if (File.Exists(args[0]))
+					{
+						if (Path.GetExtension(args[0]) == ".bin" || Path.GetExtension(args[0]) == ".sgl")
+						{
+							uploadFile(args[0]);
+						}
+						else
+						{
+							lblMessage.Text = "Only .sgl or .bin files can be uploaded";
+						}
+					}
+				}
+				catch(Exception)
+				{
+					lblMessage.Text = "Sorry. There has been an error";
+				}
+			}
+		}
+
+		public void SetLabel(string txt)
+		{
+			if (this.lblMessage.InvokeRequired)
+			{
+				lblMessage.Invoke((MethodInvoker)(() => lblMessage.Text = txt));
+				//this.Refresh();
+			}
+			else
+			{
+				this.lblMessage.Text = txt;
+			}
+		}
+
+		public void SetProgressPercentage(int perc)
+		{
+			if (this.lblMessage.InvokeRequired)
+			{
+				progressBar1.Invoke((MethodInvoker)(() => progressBar1.Value = perc));
+			}
+			else
+			{
+				progressBar1.Value = perc;
+			}
+
+		}
+
+		private void uploadFile(string filename)
+		{
+			try
+			{
+				progressBar1.Visible = true;
+				_filename = filename;
+				Thread th = new Thread(new ThreadStart(firmwareLoaderStart));
+
+				th.Start();
+				;
+			}
+			catch (Exception)
+			{
+				lblMessage.Text = "Error while uploading the firmware";
+			}
+		}
+		private void firmwareLoaderStart()
+		{
+			FirmwareLoader.UploadFirmare(_filename, this);
+		}
+
+		private void btnOpenFile_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+			openFileDialog1.Filter = "firmware files (*.sgl;*.bin)|*.sgl;*.bin|All files (*.*)|*.*";
+			openFileDialog1.RestoreDirectory = true;
+
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				uploadFile(openFileDialog1.FileName);
+			}
+		}
+	}
+}
